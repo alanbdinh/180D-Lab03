8c24bf948882bb27721fa936c32c6ac689a49c58
Alan Dinh <alanbdinh@gmail.com>
2021-10-20 21:36:20 -0700

    configured subscriber on rpi. Still no on_message callback

--- publisher.py
+++ publisher.py
@@ -1,12 +1,16 @@
 import paho.mqtt.client as mqtt
 import numpy as np
+import asyncio
 # 0. define callbacks - functions that run when events happen.
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
 	print("Connection returned result: "+str(rc))
+	for i in range(10):
+		client.publish(topic = 'aland', payload = i, qos=1)
+		print('pub loop #' + str(i) + '\n')
 # Subscribing in on_connect() means that if we lose the connection and
 # reconnect then subscriptions will be renewed.
-# client.subscribe("ece180d/test")
+# client.subscribe("aland")
 # The callback of the client when it disconnects.
 def on_disconnect(client, userdata, rc):
 	if rc != 0:
@@ -26,14 +30,18 @@ client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 client.on_message = on_message
 # 2. connect to a broker using one of the connect*() functions.
-client.connect_async('mqtt.eclipse.org')
+client.connect('mqtt.eclipseprojects.io', 1883, 5)
 # 3. call one of the loop*() functions to maintain network traffic flow with the broker.
-client.loop_start()
+client.loop_forever()
+#client.loop_start()
 # 4. use subscribe() to subscribe to a topic and receive messages.
 # 5. use publish() to publish messages to the broker.
 # payload must be a string, bytearray, int, float or None.
-for i in range(10):
-	client.publish('ece180d/test', float(np.random.random(1)), qos=1)
+# asyncio.sleep(2) #allow CONNACK time to come back from broker
+# the below loop often runs before connection state 0 is given
+# for i in range(10):
+	# client.publish(topic = 'aland', payload = i, qos=1)
+	# print('pub loop #' + str(i) + '\n')
 # 6. use disconnect() to disconnect from the broker.
-client.loop_stop()
-client.disconnect()
+#client.loop_stop()
+#client.disconnect()
